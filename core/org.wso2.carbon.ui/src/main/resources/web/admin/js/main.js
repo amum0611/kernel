6c66d7ad858b2580f9a62058e0e64f635a615d97
amani <amani@a5903396-d722-0410-b921-86c7d4935375>
2012-04-16 07:28:02 +0000

    Fix for CARBON-12667.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/kernel/trunk@124956 a5903396-d722-0410-b921-86c7d4935375

--- core/org.wso2.carbon.ui/src/main/resources/web/admin/js/main.js
+++ core/org.wso2.carbon.ui/src/main/resources/web/admin/js/main.js
@@ -1745,7 +1745,7 @@ function sessionAwareFunction(success, message, failure) {
 	        }
 	    });
     }else{
-        $.ajax({
+        jQuery.ajax({
         type:"POST",
         url:'../admin/jsp/session-validate.jsp',
         data: 'random='+random,
