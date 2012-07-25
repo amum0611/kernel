35fbb104deca84a028fac4121975f2af42041a6f
hasini <hasini@a5903396-d722-0410-b921-86c7d4935375>
2012-07-25 08:15:04 +0000

    Fixing an issue in creating a tenant which causes a login failure.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/kernel/trunk@134777 a5903396-d722-0410-b921-86c7d4935375

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/config/multitenancy/CommonLDAPRealmConfigBuilder.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/config/multitenancy/CommonLDAPRealmConfigBuilder.java
@@ -128,9 +128,10 @@ public class CommonLDAPRealmConfigBuilder implements MultiTenantRealmConfigBuild
             }
 
             //if UserDNPattern is mentioned, replace it to align with tenant's user store.
-            if (userStoreProperties.containsKey(LDAPConstants.USER_DN_PATTERN)) {
+            if (bootStrapConfig.getUserStoreProperties().containsKey(LDAPConstants.USER_DN_PATTERN)) {
                 //get userDN pattern from super tenant realm config
-                String userDNPattern = userStoreProperties.get(LDAPConstants.USER_DN_PATTERN);
+                String userDNPattern = bootStrapConfig.getUserStoreProperties().get(
+                        LDAPConstants.USER_DN_PATTERN);
                 //obtain the identifier - eg: uid={0}
                 String userIdentifier = userDNPattern.split(",")[0];
                 //build tenant specific one - eg:uid={0},ou=Users,ou=cse.org,dc=wso2,dc=org
